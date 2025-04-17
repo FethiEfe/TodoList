655c07a90fd71af49a7a1c6a1331ade7e9e3a675
FETHI AKCAY <f.akcay1@gmail.com>
2025-04-16 20:15:33 -0400

    refactoring

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,16 +3,13 @@ import TodoListItem from "./TodoListItem";
 function TodoList({todoList, onCompleteTodo}) {
   const filteredTodoList = todoList.filter(todo => !todo.isCompleted)
   return (
-    <>
     <ul>
-      {/* Added a second argument "index" to address error: 
-      "Each child in a list should have a unique "key" prop"." */}
+      
       {filteredTodoList.length === 0 ? (<p>Add todo above to get started</p>):
       (filteredTodoList.map((todo, index) => (
         <TodoListItem todo={todo} key={index} onCompleteTodo={onCompleteTodo}/>
       )))}
     </ul>
-    </>
   );
 }
 export default TodoList;
